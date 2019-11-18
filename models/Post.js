c64cdc173bdebde3b9e6ef82b39d2601ccb62ee5
isomdurm <isom@isom.tech>
2019-11-18 12:07:56 -0800

    posts crud beginning creation

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,29 @@
-Post.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+
+    event: {
+        type: Schema.Types.ObjectId,
+        ref: 'events'
+    },
+
+    body: {
+        type: String,
+        required: true
+    }, 
+
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+const Post = mongoose.model('post', PostSchema);
+
+module.exports = Post; 
