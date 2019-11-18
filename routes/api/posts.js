a0f9da6108afb995b8595268adc8615fb67d146e
isomdurm <isom@isom.tech>
2019-11-18 13:50:52 -0800

    posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,1 +1,6 @@
-posts.js
+const express = require("express");
+const router = express.Router();
+
+router.get("/test", (req, res) => res.json({ msg: "This is the events route" }));
+
+module.exports = router;
