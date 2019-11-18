16794c2c5be3b8afee8b438b6778170fd4e33601
Julia <juliawang105@gmail.com>
2019-11-18 13:55:01 -0800

    add get events route

--- routes/api/events.js
+++ routes/api/events.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Event = require("../../models/Event");
+const validateEventInput = require("../../validation/events");
 
 router.get("/test", (req, res) => res.json({ msg: "This is the events route" }));
+
+router.get("/", (req, res) => {
+    Event
+        .find()
+        .sort({ date: -1 })
+        .then(events => res.json(events))
+        .catch(err => res.status(400).json(err))
+});
+
+
 
 module.exports = router;
