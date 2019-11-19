2debf49b1c5d32f13a5a1b73d1edfae707e627e4
Julia <juliawang105@gmail.com>
2019-11-19 10:29:19 -0800

    add event actions

---
+++ frontend/src/actions/event_actions.js
@@ -0,0 +1,35 @@
+import * as EventAPIUtil from "../util/event_api_util";
+import jwt_decode from "jwt-decode";
+
+export const RECEIVE_EVENTS = "RECEIVE_EVENTS";
+export const RECEIVE_EVENT = "RECEIVE_EVENT";
+
+const receiveEvents = events => ({
+    type: RECEIVE_EVENTS,
+    events
+});
+
+const receiveEvent = event => ({
+    type: RECEIVE_EVENT,
+    event
+});
+
+export const getEvents = () => dispatch =>
+    EventAPIUtil.getEvents()
+        .then(events => dispatch(receiveEvents(events)))
+        .catch(err => console.log(err))
+
+export const getEvent = event => dispatch =>
+    EventAPIUtil.getEvents(event.id)
+        .then(event => dispatch(receiveEvent(event)))
+        .catch(err => console.log(err));
+
+export const createEvent = event => dispatch =>
+         EventAPIUtil.createEvent(event)
+           .then(event => dispatch(receiveEvent(event)))
+           .catch(err => console.log(err));
+
+export const updateEvent = event => dispatch =>
+    EventAPIUtil.updateEvent(event)
+        .then(event => dispatch(receiveEvent(event)))
+        .catch(err => console.log(err));
--- frontend/src/util/event_api_util.js
+++ frontend/src/util/event_api_util.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 export const getEvents = () => {
   return axios.get("/api/events");
 };
