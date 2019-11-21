fe7e4623dce2b88fc6262f9d536bb8c51609cda4
Fion <fionpang97@gmail.com>
2019-11-21 11:04:40 -0800

    fix event show

---
+++ frontend/src/components/reservations/reservations.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Reservations extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
+
+export default Reservations;
