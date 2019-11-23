c801de97a2f9a1a854cf830ac5db6c34c592e060
Julia <juliawang105@gmail.com>
2019-11-22 18:11:41 -0800

    fetch event names for user profile

--- frontend/src/components/users/user_show.jsx
+++ frontend/src/components/users/user_show.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReservationItem from '../reservations/reservation_item';
+import { Link } from 'react-router-dom';
 
 class Users extends React.Component {
   constructor(props) {
@@ -20,12 +21,17 @@ class Users extends React.Component {
   
 
     console.log(this.props);
-
-    this.props.fetchUserReservations(this.props.user.email)
-      .then(() => {
-        this.props.fetchUserEvents(this.props.user.id)
-          .then(() => this.setState({ loaded: true }));
-      });
+    this.props.fetchUserEvents(this.props.user.id)
+    .then(() => {
+      this.props.fetchUserReservations(this.props.user.email)
+        .then((res) => {
+          let reservations = res.reservations.data;
+          // console.log(reservations)
+          for (let i = 0; i < reservations.length; i++) {
+            this.props.getEvent(reservations[i].event)
+          }
+        })
+    }).then(() => this.setState({ loaded: true }));;
   }
 
   update() {
@@ -39,14 +45,16 @@ class Users extends React.Component {
     if (!this.state.loaded) {
       return null;
     }
-
-    let reservations = this.props.reservations.all;
+    // debugger
+    // let reservations = this.props.reservations.all;
     let user = this.props.user;
-    let events = this.props.events.all;
+    let hostedEvents = this.props.events.all;
+    let inviteEvents = this.props.events.user;
+    
 
     return (
       <div className="reservations">
-        <div>
+        {/* <div>
           {reservations.map(reservation => {
             return (
               <ReservationItem
@@ -57,11 +65,43 @@ class Users extends React.Component {
               />
             );
           })}
-        </div>
+        </div> */}
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+
+        <h1>Hosted Events</h1>
         <div>
-          {events.map(event => {
+          {hostedEvents.map(event => {
             return (
-              <div key={event._id}>{event.name}</div>
+              <div key={event._id}><Link to={`/events/${event._id}`}>{event.name}</Link>
+                <div>Description: {event.body}</div> 
+                <br/>
+                <div>Where: {event.location}</div>
+                <br/>
+                <div>When:{event.time}</div>
+                <br/>
+                <br />
+              </div>
+              
+            );
+          })}
+        </div>
+
+        <h1>Invited Events</h1>
+        <div>
+          {inviteEvents.map(event => {
+            return (
+              <div key={event._id}><Link to={`/events/${event._id}`}>{event.name}</Link>
+                <div>Description: {event.body}</div>
+                <br />
+                <div>Where: {event.location}</div>
+                <br />
+                <div>When:{event.time}</div>
+                <br />
+                <br />
+              </div>
             );
           })}
         </div>
