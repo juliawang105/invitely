594562c087620bcd4682d842af89a96a699023b5
danna-aa <danna@berkeley.edu>
2019-11-23 21:20:26 -0800

    Break word

--- frontend/src/components/posts/post_item.jsx
+++ frontend/src/components/posts/post_item.jsx
@@ -7,9 +7,10 @@ class PostItem extends React.Component {
   render() {
     // console.log(this.props.post)
     let date = new Date(this.props.post.date).toDateString();
+    let time = new Date(this.props.post.date).toLocaleTimeString();
     return (
       <div>
-        {date}
+        {date} {time}
         <br/>
         {this.props.post.authorFirst} {this.props.post.authorLast}: {this.props.post.body}
         <br/>
