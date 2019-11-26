75ff209fffbacdd5752dd639c66f10338bef6e0d
danna-aa <danna@berkeley.edu>
2019-11-25 17:16:13 -0800

    git ignore

---
+++ config/aws.js
@@ -0,0 +1,8 @@
+module.exports = {
+    accessKeyId:
+      "AKIAJPEMGBGZHG763VSQ",
+    secretKey:
+      "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",
+    bucketName:
+      "invitely-event-images"
+}
---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// /config/keys.js
+
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dev:eEzyXLNT9A2vDtHU@invitely-sszxq.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "Z5PKJ0J2HA",
+    moduleResolution: "node"
+};
+  
