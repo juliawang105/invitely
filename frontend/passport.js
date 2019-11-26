866056c8ed746136ba0b11f97f055475189315a5
isomdurm <isom@isom.tech>
2019-11-26 11:31:21 -0800

    environments fix

--- frontend/passport.js
+++ frontend/passport.js
@@ -2,17 +2,15 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
-// const keys = require("../config/keys");
+const keys = require("../config/keys");
 
 const options = {};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-options.secretOrKey = process.env.secretOrKey;
+options.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
   passport.use(
     new JwtStrategy(options, (jwt_payload, done) => {
-      console.log("ok");
-      console.log(jwt_payload);
       
       User.findById(jwt_payload.id)
         .then(user => {
