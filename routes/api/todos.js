db982218613c9745cf85f8a04004a2817588a7f7
Fion <fionpang97@gmail.com>
2019-11-28 14:01:42 -0800

    finish backend routes for todos

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -39,5 +39,39 @@ router.post(
   }
 );
 
+router.delete("/:id", (req, res) => {
+  Todo.findByIdAndRemove(req.params.id, function (err, todo) {
+    if (err)
+      return res.status(500).send("There was a problem deleting the task.");
+    res.status(200).send(todo);
+  });
+});
+
+router.get("/:id", (req, res) => { 
+  Todo.findById(req.params.id)
+    .then(todo => res.json(todo))
+    .catch(err => res.status(400).json(err));
+});
+
+router.patch("/:id", (req, res, next) => {
+  const { errors, isValid } = validateTodoInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+
+  Todo.findOneAndUpdate(req.params.id,
+    req.body,
+    // console.log(req.body),
+    { new: true })
+    .then((todo) => {
+      console.log(todo);
+      res.json(todo);
+    });
+
+  }
+);
+
 
 module.exports = router;
