32fcb8a75f6471060a8e8e32c535b1cdb9440a4a
isomdurm <isom@isom.tech>
2019-12-10 21:49:54 -0800

    fixing css stylings of user show

--- frontend/src/components/users/user_show.jsx
+++ frontend/src/components/users/user_show.jsx
@@ -73,24 +73,29 @@ class Users extends React.Component {
                 <div className="user-event" key={event._id}>
                   <Link to={`/events/${event._id}`}>
                     <div className="user-event-details">
-                      <img src={`${event.image_url}`} />
-                      <div className="text">
-                        <div className="text-title">
-                          <h3>Event</h3>
+                      <div className="grid-container">
+                        <div className="left">
+                          <img height="auto" width="150px" src={`${event.image_url}`} />
                         </div>
-                        <div className="text-info">{event.name}</div>
-                      </div>
 
-                      <div className="text">
-                        <div className="text-title">Where</div>
-                        <div className="text-info">{event.location}</div>
-                      </div>
+                        <div className="right">
+                          <div className="text">
+                            <div className="text-title">Event</div>
+                            <div className="text-info">{event.name}</div>
+                          </div>
 
-                      <div className="text">
-                        <div className="text-title">When</div>
-                        <div className="text-info">
-                          {new Date(event.time).toDateString()}{" "}
-                          {new Date(event.time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}
+                          <div className="text">
+                            <div className="text-title">Where</div>
+                            <div className="text-info">{event.location}</div>
+                          </div>
+
+                          <div className="text">
+                            <div className="text-title">When</div>
+                            <div className="text-info">
+                              {new Date(event.time).toDateString()}{" "}
+                              {new Date(event.time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
