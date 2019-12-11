74796d4049e47d98bac06a090eed778cde03fbed
danna-aa <danna@berkeley.edu>
2019-12-11 11:12:14 -0800

    Default image user show

--- frontend/src/components/users/user_show.jsx
+++ frontend/src/components/users/user_show.jsx
@@ -58,26 +58,53 @@ class Users extends React.Component {
       return (new Date(a.time) - new Date(b.time));
     });
 
-
-
-    
+    let noEventMessage
+    if (hostedEvents.length === 0) {
+      noEventMessage = 
+        <div className="user-event invited-event">
+          No Events Yet!
+        </div>
+    }
+    let noneInvitedMessage
+    if (inviteEvents.length === 0) {
+      noneInvitedMessage = 
+        <div className="user-event invited-event">
+          No Invites Yet!
+        </div>
+    }
 
     return (
       <div className="user-show">
         <div className="user-background"></div>
         <div className="user-show-box">
-
           <div className="invite-host">
             <h1>My Invitations</h1>
+            {noneInvitedMessage}
             {inviteEvents.map(event => {
+              let image;
+              if (!event.image_url) {
+                image = (
+                  <img
+                    src="gif_011.gif"
+                    className="user-show-event-image"
+                    alt="dinner_placeholder"
+                  />
+                );
+              } else {
+                image = (
+                  <img
+                    height="auto"
+                    src={`${event.image_url}`}
+                    className="user-show-event-image"
+                  />
+                );
+              }
               return (
                 <div className="user-event invited-event" key={event._id}>
                   <Link to={`/events/${event._id}`}>
                     <div className="user-event-details">
                       <div className="grid-container">
-                        <div className="left">
-                          <img height="auto" width="150px" src={`${event.image_url}`} />
-                        </div>
+                        <div className="left">{image}</div>
                         <div className="right">
                           <div className="text">
                             <div className="text-title">Event</div>
@@ -93,7 +120,10 @@ class Users extends React.Component {
                             <div className="text-title">When</div>
                             <div className="text-info">
                               {new Date(event.time).toDateString()}{" "}
-                              {new Date(event.time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}
+                              {new Date(event.time).toLocaleTimeString([], {
+                                hour: "2-digit",
+                                minute: "2-digit"
+                              })}
                             </div>
                           </div>
                         </div>
@@ -104,36 +134,53 @@ class Users extends React.Component {
               );
             })}
           </div>
-          
+
           <div className="invite-host">
             <h1>My Hosted Events</h1>
+            {noEventMessage}
             {hostedEvents.map(event => {
+              let image;
+              if (!event.image_url) {
+                image = (
+                  <img
+                    src="gif_012.gif"
+                    className="user-show-event-image"
+                    alt="dinner_placeholder"
+                  />
+                );
+              } else {
+                image = (
+                  <img
+                    height="auto"
+                    src={`${event.image_url}`}
+                    className="user-show-event-image"
+                  />
+                );
+              }
               return (
                 <div className="user-event hosted-event" key={event._id + 1}>
                   <Link to={`/events/${event._id}`}>
                     <div className="user-event-details">
-                      <div className="grid-container">
-                        <div className="left">
-                          <img src={`${event.image_url}`} />
-                        </div>
-                        <div className="right">
-                          <div className="text">
-                            <div className="text-title">Event</div>
-                            <div className="text-info">{event.name}</div>
-                          </div>
+                      <div className="left">{image}</div>
 
-                          <div className="text">
-                            <div className="text-title">Where</div>
-                            <div className="text-info">{event.location}</div>
-                          </div>
+                      <div className="text">
+                        <div className="text-title">Event</div>
+                        <div className="text-info">{event.name}</div>
+                      </div>
 
-                          <div className="text">
-                            <div className="text-title">When</div>
-                            <div className="text-info">
-                              {new Date(event.time).toDateString()}{" "}
-                              {new Date(event.time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}
-                            </div>
-                          </div>
+                      <div className="text">
+                        <div className="text-title">Where</div>
+                        <div className="text-info">{event.location}</div>
+                      </div>
+
+                      <div className="text">
+                        <div className="text-title">When</div>
+                        <div className="text-info">
+                          {new Date(event.time).toDateString()}{" "}
+                          {new Date(event.time).toLocaleTimeString([], {
+                            hour: "2-digit",
+                            minute: "2-digit"
+                          })}
                         </div>
                       </div>
                     </div>
