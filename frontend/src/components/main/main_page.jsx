ba00db7501e59dfcd89168b1647b868d90857e65
danna-aa <danna@berkeley.edu>
2019-12-10 19:08:02 -0800

    Fix

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -35,7 +35,8 @@ class MainPage extends React.Component {
           </h1>
           <div className="peek">
             <div className="down-arrow hvr-hang">
-              <i className="fas fa-sort-down"></i>
+              <i className="fas fa-arrow-down"></i> Scroll Down for More Info{" "}
+              <i className="fas fa-arrow-down"></i>
             </div>
           </div>
         </div>
@@ -50,14 +51,18 @@ class MainPage extends React.Component {
               </div>
             </div>
             <div>
-              <img src="pic_001.jpeg" alt="2" className="main-image round-image" />
+              <img
+                src="pic_001.jpeg"
+                alt="2"
+                className="main-image round-image"
+              />
               {/* <img src="gif_004.gif" alt="2" className="main-image" /> */}
               {/* <img src="gif_008.gif" alt="2" className="main-image" /> */}
             </div>
           </div>
           <div className="peek alt">
             <div className="down-arrow hvr-hang">
-              <i className="fas fa-sort-down"></i>
+              <i className="fas fa-arrow-down"></i>
             </div>
           </div>
         </div>
@@ -66,7 +71,11 @@ class MainPage extends React.Component {
           <div className="section">
             <div>
               {/* <img src="pic_005.jpg" alt="3" className="main-image" /> */}
-              <img src="gif_001.gif" alt="3" className="main-image round-image" />
+              <img
+                src="gif_001.gif"
+                alt="3"
+                className="main-image round-image"
+              />
             </div>
             <div className="text-box alt">
               <h2 className="sub-header">Invite</h2>
@@ -77,7 +86,7 @@ class MainPage extends React.Component {
           </div>
           <div className="peek">
             <div className="down-arrow hvr-hang">
-              <i className="fas fa-sort-down"></i>
+              <i className="fas fa-arrow-down"></i>
             </div>
           </div>
         </div>
@@ -91,7 +100,11 @@ class MainPage extends React.Component {
               </div>
             </div>
             <div>
-              <img src="pic_002.jpeg" alt="pic_02" className="main-image round-image" />
+              <img
+                src="pic_002.jpeg"
+                alt="pic_02"
+                className="main-image round-image"
+              />
               {/* <img src="gif_002.gif" alt="4" className="main-image" /> */}
             </div>
           </div>
@@ -123,9 +136,9 @@ class MainPage extends React.Component {
                   <h2>Log In</h2>
                 </Link>
               </div>
-              <br/>
+              <br />
               <div className="button-box" onClick={e => this.loginDemoUser()}>
-                  <h2>Try Demo</h2>
+                <h2>Try Demo</h2>
               </div>
             </div>
           </div>
@@ -141,11 +154,7 @@ class MainPage extends React.Component {
           <div className="section">
             <div className="dev-box">
               <a href="https://www.linkedin.com/in/isom-durm/">
-                <img
-                  src="dev-isom.jpg"
-                  alt="Isom Durm"
-                  className="dev-image"
-                />
+                <img src="dev-isom.jpg" alt="Isom Durm" className="dev-image" />
               </a>
               <div className="sub-tagline">
                 <a href="https://www.linkedin.com/in/isom-durm/">Isom Durm</a>
